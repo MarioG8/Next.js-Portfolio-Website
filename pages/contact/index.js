f6c83d43c91510480f18617316ed7d6048d8ea12
MarioG8 <golikmariusz7@gmail.com>
2021-06-06 16:20:04 +0200

    Added styles to contact page

--- pages/contact/index.js
+++ pages/contact/index.js
@@ -1,20 +1,22 @@
+import styles from "@/styles/Contact.module.css";
+
 function ContactPage() {
   return (
-    <main id="contact">
-      <h1 class="lg-heading">
+    <main className={styles.contact}>
+      <h1 className="lg-heading">
         Contact
-        <span class="text-secondary">Me</span>
+        <span className="text-secondary">Me</span>
       </h1>
-      <h2 class="sm-heading">This is how you can reach me...</h2>
-      <div class="boxes">
+      <h2 className="sm-heading">This is how you can reach me...</h2>
+      <div className="boxes">
         <div>
-          <span class="text-secondary">Email: </span> johndoe@test.com
+          <span className="text-secondary">Email: </span> johndoe@test.com
         </div>
         <div>
-          <span class="text-secondary">Phone: </span> (555) 555-5555
+          <span className="text-secondary">Phone: </span> (555) 555-5555
         </div>
         <div>
-          <span class="text-secondary">Address: </span> 50 Main st Boston MA
+          <span className="text-secondary">Address: </span> 50 Main st Boston MA
           02101
         </div>
       </div>
