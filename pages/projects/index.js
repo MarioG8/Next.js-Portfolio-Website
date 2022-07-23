78c03ba47ee9e0c73a57454b9ece712e98201ee4
MarioG8 <golikmariusz7@gmail.com>
2022-07-23 13:45:49 +0200

    added miniature

--- pages/projects/index.js
+++ pages/projects/index.js
@@ -45,7 +45,7 @@ function ProjectsPage() {
           git="https://github.com/MarioG8/React-Stoa-Website-"
         />
         <Card
-          img="/images/code_011.jpg"
+          img="/images/404_006.jpg"
           date="15 June 2021"
           lang="Next.js"
           title="Portfolio Website"
