d34ec927df1307d3990ac4fa4c84b0a709947668
MarioG8 <golikmariusz7@gmail.com>
2022-11-07 11:32:22 +0100

    changed the foto

--- components/Menu.js
+++ components/Menu.js
@@ -23,7 +23,7 @@ function Header(props) {
         <div className={menu ? "menu-branding" : "menu-branding show"}>
           <div className="portrait-img-2">
             <Image
-              src="/images/mario_007.jpg"
+              src="/images/mario_t-1000.jpg"
               width={280}
               height={400}
               quality={100}
